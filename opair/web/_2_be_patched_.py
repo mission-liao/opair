70feab6341d0c7ee6f43049c2fb68dd57c6b33b0
mission.liao <missionaryliao@gmail.com>
2013-07-20 00:58:56 +0800

    init web front-end
    many dev-code should be cleaned later.

---
+++ opair/web/_2_be_patched_.py
@@ -0,0 +1,11 @@
+'''
+Created on Jul 19, 2013
+
+@author: Mission Liao
+'''
+
+# TODO: remember to clear these secret info before commit
+
+_cookie_patched = "__TODO_PATCH_WITH_SOME_RADOM_STRING__"
+_facebook_api_key_patched = "__TODO_NUMBER_TO_BE_PATCHED__"
+_facebook_secret_patched = "__TODO_HEX_NUMBER_TO_BE_PATCHED__"
