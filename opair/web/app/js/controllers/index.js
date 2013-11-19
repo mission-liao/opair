87c4d36473da833af81537aaf64b01faf7a07c04
mission.liao <missionaryliao@gmail.com>
2013-11-19 22:31:36 +0800

    replace '.' to '-'
    '.' is object-access operator in javascript

--- opair/web/app/js/controllers/index.js
+++ opair/web/app/js/controllers/index.js
@@ -2,17 +2,17 @@ define(["angular"], function (ng) {
     "use strict";
 
     return ng.module("webApp.controllers", [])
-        .controller("ctrl.login", ["$scope", "$injector", "$http", function ($scope, $injector, $http) {
+        .controller("ctrl_login", ["$scope", "$injector", "$http", function ($scope, $injector, $http) {
             require(["controllers/login/login"], function (c) {
                 $injector.invoke(c, this, {"$scope": $scope, "$http": $http});
             });
         }])
-        .controller("ctrl.sign_up", ["$scope", "$injector", function ($scope, $injector) {
+        .controller("ctrl_signUp", ["$scope", "$injector", function ($scope, $injector) {
             require(["controllers/sign_up"], function (c) {
                 $injector.invoke(c, this, {"$scope": $scope});
             });
         }])
-        .controller("ctrl.psswd", ["$scope", "$injector", "$element", function ($scope, $injector, $elm) {
+        .controller("ctrl_psswd", ["$scope", "$injector", "$element", function ($scope, $injector, $elm) {
             require(["controllers/login/psswd"], function (c) {
                 $injector.invoke(c, this, {"$scope": $scope, "$elm": $elm});
             });
