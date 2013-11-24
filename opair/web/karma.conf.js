cf1264ed271182e4b981ecfa0f44c53a4e2d6d08
mission.liao <missionaryliao@gmail.com>
2013-11-24 21:14:04 +0800

    fix karma
    - requirejs + angularjs

--- opair/web/karma.conf.js
+++ opair/web/karma.conf.js
@@ -13,24 +13,41 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/scripts/*.js',
-      'app/scripts/**/*.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/jquery/jquery.js',
+
+      // ---- bootstrap js files, need to handle include order manually.
+      'app/bower_components/bootstrap-sass/js/dropdown.js',
+
+      'app/bower_components/restangular/dist/restangular.min.js',
+      // ---- files needs to be loaded by requirejs
+      'app/bower_components/requirejs/require.js',
+      {pattern: 'app/js/*.js', included: false},
+      {pattern: 'app/js/**/*.js', included: false},
+      {pattern: 'app/bower_components/requirejs-domready/domReady.js', included: false},
+      {pattern: 'app/bower_components/lodash/dist/lodash.compat.min.js', included: false},
+
+      // config-file for requirejs when testing
+      'test/main.js',
+
+      // test-spec
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
-    exclude: [],
+    exclude: [
+      'app/js/main.js'
+    ],
 
     // web server port
     port: 8080,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -50,6 +67,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+
   });
 };
---
+++ opair/web/test/main.js
@@ -0,0 +1,45 @@
+require.config({
+    baseUrl: '/base/app/js',
+
+    paths: {
+        angular: '../bower_components/angular/angular',
+        angular_cookies: '../bower_components/angular-cookies/angular-cookies',
+        angular_sanitize: '../bower_components/angular-sanitize/angular-sanitize',
+        angular_route: '../bower_components/angular-route/angular-route',
+        jquery: '../bower_components/jquery/jquery',
+        bootstrap: '../bower_components/bootstrap-sass/js',
+        requirejs_domready: '../bower_components/requirejs-domready/domReady',
+        restangular: '../bower_components/restangular/dist/restangular.min',
+        lodash: '../bower_components/lodash/dist/lodash.compat.min'
+    },
+
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'angular_cookies': {
+            deps: ['angular']
+        },
+        'angular_sanitize': {
+            deps: ['angular']
+        },
+        'angular_route': {
+            deps: ['angular']
+        },
+        'restangular': {
+            deps: ['lodash'],
+        },
+        // not beautiful, shim didn't allow wildchar
+        // we have to declare deps for each bootstrap module
+        // we included.
+        'bootstrap/dropdown': {
+            deps: ['jquery'],
+            exports: '$.fn.popover'
+        },
+    },
+
+    deps: [ 
+      'app'
+    ],
+});
+
