b8bd548f908b2ad68679639e49a17870776af4ba
mission.liao <missionaryliao@gmail.com>
2013-12-02 19:51:30 +0800

    refine the sync/async js loading code
    we can force to use sync loading by passing bSync as true when calling
    _invoke

--- opair/web/app/js/controllers/index.js
+++ opair/web/app/js/controllers/index.js
@@ -1,8 +1,9 @@
-define(['angular'], function (ng) {
+define(['angular', 'controllers/err'], function (ng) {
     'use strict';
 
-    var _invoke = function (ctrl, pThis, param, inj, JSL) {
-        if (JSL.is_sync()) {
+
+    var _invoke = function (ctrl, pThis, param, inj, bSync) {
+        if (bSync) {
             var c = require(ctrl);
             inj.invoke(c, pThis, param);
         } else {
@@ -14,10 +15,16 @@ define(['angular'], function (ng) {
 
     return ng.module('webApp.controllers', [])
         .controller('ctrl_login', ['service_JSLoad', '$scope', '$injector', 'ApiRestangular', function (JSL, $scope, $injector, ApiRestangular) {
-            _invoke('controllers/login', this, {'$scope': $scope, 'ApiRestangular': ApiRestangular}, $injector, JSL);
+            _invoke('controllers/login', this, {'$scope': $scope, 'ApiRestangular': ApiRestangular}, $injector, JSL.is_sync());
         }])
         .controller('ctrl_signUp', ['service_JSLoad', '$scope', '$injector', 'RRestangular', function (JSL, $scope, $injector, RRestangular) {
-            _invoke('controllers/sign_up', this, {'$scope': $scope, 'RRestangular': RRestangular}, $injector, JSL);
+            _invoke('controllers/sign_up', this, {'$scope': $scope, 'RRestangular': RRestangular}, $injector, JSL.is_sync());
+        }])
+        .controller('ctrl_home', ['service_JSLoad', '$scope', '$injector', function (JSL, $scope, $injector) {
+            _invoke('controllers/home', this, {'$scope': $scope}, $injector, JSL.is_sync());
+        }])
+        .controller('ctrl_err', ['service_JSLoad', '$scope', '$injector', function (JSL, $scope, $injector) {
+            _invoke('controllers/err', this, {'$scope': $scope}, $injector, JSL.is_sync());
         }]);
 });
 
