cfe1d3e52a81bb3ea9588ab3af10dddeda7fec52
mission.liao <missionaryliao@gmail.com>
2013-12-02 19:50:17 +0800

    tmp dev changes
    - move views/login.html to views/login/form.html, the original view is
    replaced with views/top_r.html
    - add error view: views/error.html to show http 401 or 5xx error.

--- opair/web/app/js/app.js
+++ opair/web/app/js/app.js
@@ -14,17 +14,53 @@ define([
         'ngSanitize',
         'ngRoute',
         'restangular',
+
         'webApp.controllers',
-        'webApp.services'
+        'webApp.services',
     ])
     .config(function ($routeProvider) {
         $routeProvider
         .when('/', {
+            templateUrl: 'views/home.html',
+        })
+        .when('/intro', {
             templateUrl: 'views/intro.html',
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+        })
+        .when('/error', {
+            templateUrl: 'views/error.html',
         })
         .otherwise({
             redirectTo: '/'
         });
+    })
+    .run(function ($rootScope, ApiRestangular) {
+        // init UI state
+        $rootScope.ui_state = {};
+        $rootScope.ui_state.login = false;
+
+        // init http-response cache, for error
+        $rootScope.http_response = {};
+        // init an invalid value to status code
+        $rootScope.http_response.status = 0;
+
+        // init global user object
+        $rootScope.user = {};
+        // query user-email from server, once queried, which
+        // means we've login.
+        var user = ApiRestangular.one('login');
+        user.get().then(
+            function (data) {
+                $rootScope.user.email = data.email;
+            },
+            function (err) {
+                $rootScope.ui_state.login = false;
+            }
+        );
+
+        // TODO: check cookie for user-id
     });
 });
 
